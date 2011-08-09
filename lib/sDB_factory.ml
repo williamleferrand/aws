9d05720ad9d000dc7b49a13ebebec449a87bf07f
barko <barko@nowhere.net>
2011-08-09 15:59:56 -0700

    - conforming to the MJ80 line rule - factoring 'if encoded ...' into pair of enc/dec functions - batch_put_attributes's values are now optional

--- lib/sDB_factory.ml
+++ lib/sDB_factory.ml
@@ -74,47 +74,59 @@ struct
       
   let error_msg code' body =
     match X.xml_of_string body with
-      | X.E ("Response",_,
-             (X.E ("Errors",_,
-                   [
-                     X.E ("Error",_,[
-                       X.E ("Code",_,[X.P code]);
-                       X.E ("Message",_,[X.P message]); 
-                       _ 
-                     ])]))::_) -> `Error (code, message)
+      | X.E ("Response",_, (
+               X.E ("Errors",_, [
+                      X.E ("Error",_,[
+                             X.E ("Code",_,[X.P code]);
+                             X.E ("Message",_,[X.P message]); 
+                             _ 
+                           ]
+                          )
+                    ]
+                   )
+             ) :: _ ) -> `Error (code, message)
       | _ -> `Error ("unknown", body)
 
+
+  let b64dec_if encoded s =
+    if encoded then
+      Util.base64_decoder s
+    else
+      s
+
+  let b64enc_if encode s =
+    if encode then
+      Util.base64 s
+    else
+      s
 
   let domain_of_xml = function 
     | X.E ("DomainName", _, [ X.P domain_name ]) -> domain_name 
     | _ -> raise (Error "ListDomainsResult.domain")
 
   let list_domains_response_of_xml = function 
-    | X.E ("ListDomainsResponse", _, 
-           [ 
+    | X.E ("ListDomainsResponse", _, [ 
              X.E ("ListDomainsResult", _, domains); 
              _ ]) -> List.map domain_of_xml domains
     | _ -> raise (Error "ListDomainsResult")
     
   let attributes_of_xml encoded = function 
-    | X.E ("Attribute", _, 
-           [
+    | X.E ("Attribute", _, [
              X.E ("Name", _, [ X.P name ]); 
              X.E ("Value", _, [ X.P value ]); 
-           ]) -> ((if encoded then Util.base64_decoder name else name),  
-                  (if encoded then Some (Util.base64_decoder value) else Some value))
+           ]) -> 
+        b64dec_if encoded name,  Some (b64dec_if encoded value)
 
-    | X.E ("Attribute", _, 
-           [
+    | X.E ("Attribute", _, [
              X.E ("Name", _, [ X.P name ]); 
              X.E ("Value", _, [ ]); 
-           ]) -> ((if encoded then Util.base64_decoder name else name), None)
+           ]) -> 
+        b64dec_if encoded name, None
 
     | _ -> raise (Error "Attribute 1")
 
   let get_attributes_response_of_xml encoded = function 
-    | X.E ("GetAttributesResponse", _, 
-           [ 
+    | X.E ("GetAttributesResponse", _, [ 
              X.E ("GetAttributesResult", _, attributes); 
              _; 
            ]) -> List.map (attributes_of_xml encoded) attributes
@@ -124,33 +136,32 @@ struct
   let attrs_of_xml encoded = function 
     | X.E ("Attribute", _ , children) -> 
       ( match children with 
-        | [
-          X.E ("Name", _, [ X.P name ]) ;
-          X.E ("Value", _, [ X.P value ]) ;
-        ] ->  (if encoded then Util.base64_decoder name else name), (Some  (if encoded then Util.base64_decoder value else value))
-        | [
-          X.E ("Name", _, [ X.P name ]) ;
-          X.E ("Value", _, [ ]) ;
-        ] -> (if encoded then Util.base64_decoder name else name), None  
-        | l -> failwith (Printf.sprintf "fat list %d" (List.length l)) 
- )    
+        | [ X.E ("Name", _, [ X.P name ]) ;
+            X.E ("Value", _, [ X.P value ]) ;
+          ] ->  b64dec_if encoded name, Some (b64dec_if encoded value)
+        | [ X.E ("Name", _, [ X.P name ]) ;
+            X.E ("Value", _, [ ]) ;
+          ] -> b64dec_if encoded name, None  
+        | l -> raise (Error (sprint "fat list %d" (List.length l))) 
+      )    
     | _ -> raise (Error "Attribute 3")
 
   let rec item_of_xml encoded acc token = function 
     | [] -> (acc, token)
-    | X.E ("Item", _, (X.E ("Name", _, [ X.P name ]) :: attrs)) :: nxt -> item_of_xml encoded (((if encoded then Util.base64_decoder name else name), (List.map (attrs_of_xml encoded) attrs)) :: acc) token nxt
+    | X.E ("Item", _, (X.E ("Name", _, [ X.P name ]) :: attrs)) :: nxt -> 
+        let a = List.map (attrs_of_xml encoded) attrs in
+        item_of_xml encoded (((b64dec_if encoded name), a) :: acc) token nxt
     | X.E ("NextToken", _, [ X.P next_token ]) :: _ -> acc, (Some next_token) 
     | _ -> raise (Error "Item")
       
   let select_of_xml encoded = function 
-    | X.E ("SelectResponse", _,
-           [
+    | X.E ("SelectResponse", _, [
              X.E ("SelectResult", _, items); 
              _ ;
            ]) -> item_of_xml encoded [] None items
     | _ -> raise (Error "SelectResponse")
 
-(* list all domains *)
+  (* list all domains *)
 
   let list_domains creds ?token () = 
     let url, params = signed_request creds 
@@ -165,132 +176,150 @@ struct
        return (`Ok (list_domains_response_of_xml xml))
     with HC.Http_error (code, _, body) ->  return (error_msg code body)
 
-(* create domain *)
+  (* create domain *)
 
   let create_domain creds name = 
-    let url, params = signed_request creds
-      [
-        "Action", "CreateDomain" ; 
-        "DomainName", name
-      ] in
+    let url, params = signed_request creds [
+      "Action", "CreateDomain" ; 
+      "DomainName", name
+    ] in
     
     try_lwt 
-       lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
-       
-       return `Ok
-    with HC.Http_error (code, _, body) ->  return (error_msg code body)
+      lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
+      return `Ok
+    with HC.Http_error (code, _, body) -> 
+      return (error_msg code body)
 
-(* delete domain *)
+  (* delete domain *)
 
   let delete_domain creds name = 
-    let url, params = signed_request creds
-      [
-        "Action", "DeleteDomain" ; 
-        "DomainName", name
-      ] in
+    let url, params = signed_request creds [
+      "Action", "DeleteDomain" ; 
+      "DomainName", name
+    ] in
     
     try_lwt 
        lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
-       
        return `Ok
-    with HC.Http_error (code, _, body) ->  return (error_msg code body)
+    with HC.Http_error (code, _, body) ->  
+      return (error_msg code body)
 
-(* put attributes *)
+  (* put attributes *)
   
   let put_attributes ?(replace=false) ?(encode=true) creds domain item attrs = 
-    let _, attrs' = 
-      List.fold_left 
-        (fun (i, acc) ->  
-          function 
-            | (name, None) -> 
-              (i+1), ((sprint "Attribute.%d.Name" i, (if encode then Util.base64 name else name))
-                      :: (sprint "Attribute.%d.Value" i, "")
-                      :: (if replace then 
-                          (sprint "Attribute.%d.Replace" i, "true") :: acc 
-                        else acc))
-            | (name, Some value) -> 
-              (i+1), ((sprint "Attribute.%d.Name" i, (if encode then Util.base64 name else name)) 
-                      :: (sprint "Attribute.%d.Value" i, (if encode then Util.base64 value else value))
-                      :: (if replace then 
-                          (sprint "Attribute.%d.Replace" i, "true") :: acc 
-                        else acc))) (1, []) attrs in
+    let _, attrs' = List.fold_left (
+      fun (i, acc) (name, value_opt) ->  
+        let value_s = 
+          match value_opt with
+            | Some value -> b64enc_if encode value
+            | None -> ""
+        in
+        let value_p = sprint "Attribute.%d.Value" i, value_s in
+        let name_p = sprint "Attribute.%d.Name" i, b64enc_if encode name in
+        let acc = 
+          name_p :: value_p :: (
+            if replace then 
+              (sprint "Attribute.%d.Replace" i, "true") :: acc 
+            else 
+              acc
+          ) in
+        i+1, acc
+    ) (1, []) attrs in
     let url, params = signed_request creds
       (("Action", "PutAttributes") 
        :: ("DomainName", domain)
-       :: ("ItemName", (if encode then Util.base64 item else item))
+       :: ("ItemName", b64enc_if encode item)
        :: attrs') in 
     try_lwt 
-       lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
-       
-       return `Ok
+      lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
+      
+      return `Ok
     with HC.Http_error (code, _, body) -> return (error_msg code body)
 
-(* batch put attributes *)
+  (* batch put attributes *)
       
   let batch_put_attributes ?(replace=false) ?(encode=true) creds domain items =
-    let _, attrs' = 
-      List.fold_left 
-        (fun (i, acc) (name, attrs) -> 
-          let _, acc = (List.fold_left 
-                          (fun (j, acc) (name, value) -> 
-                            (j+1), ((sprint "Item.%d.Attribute.%d.Name" i j,  (if encode then Util.base64 name else name))
-                                    :: (sprint "Item.%d.Attribute.%d.Value" i j, (if encode then Util.base64 value else value))
-                                    :: (if replace then (sprint "Item.%d.Attribute.%d.Replace" i j, "true") :: acc else acc)
-                                      
-                             )) (1, (sprint "Item.%d.ItemName" i, (if encode then Util.base64 name else name)) :: acc) attrs) in 
-        (i+1), acc)
-        (1, []) items in 
-        
+    let _, attrs' = List.fold_left 
+      (fun (i, acc) (item_name, attrs) -> 
+         let item_name_p = sprint "Item.%d.ItemName" i, b64enc_if encode item_name in
+         let _, acc = List.fold_left (
+           fun (j, acc) (name, value_opt) -> 
+             let name_p = sprint "Item.%d.Attribute.%d.Name" i j, 
+               b64enc_if encode name in
+             let value_s =
+               match value_opt with
+                 | Some value -> b64enc_if encode value
+                 | None -> "" in
+             let value_p = sprint "Item.%d.Attribute.%d.Value" i j, value_s in
+             let acc' = name_p :: value_p :: 
+               if replace then 
+                  (sprint "Item.%d.Attribute.%d.Replace" i j, "true") :: acc 
+                else 
+                  acc
+             in
+             j+1, acc'
+         ) (1, item_name_p :: acc) attrs in 
+         i+1, acc
+      ) (1, []) items in 
+    
     let url, params = signed_request creds
       (("Action", "BatchPutAttributes") 
        :: ("DomainName", domain)
        :: attrs') in 
     try_lwt 
-       lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
-       
-       return `Ok
+      lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
+      
+      return `Ok
     with HC.Http_error (code, _, body) ->  return (error_msg code body)
     
-
-    
-(* get attributes *)
+  (* get attributes *)
 
   let get_attributes ?(encoded=true) creds domain ?attribute item = 
-    let url, params = signed_request creds
-      (("Action", "GetAttributes") 
-       :: ("DomainName", domain)
-       :: ("ItemName", (if encoded then Util.base64 item else item))
-       :: (match attribute with 
-         | None -> [] 
-         | Some attribute_name -> [ "AttributeName", (if encoded then Util.base64 attribute_name else attribute_name) ])) in 
+    let attribute_name_p =
+      match attribute with 
+        | None -> [] 
+        | Some attribute_name -> 
+            [ "AttributeName", (b64enc_if encoded attribute_name) ]
+    in
+    let url, params = signed_request creds (
+      ("Action", "GetAttributes") :: 
+        ("DomainName", domain) :: 
+        ("ItemName", b64enc_if encoded item) ::
+        attribute_name_p
+    ) in
     try_lwt 
-       lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
-       
-       let xml = X.xml_of_string body in
-       return (`Ok (get_attributes_response_of_xml encoded xml))
+      lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
+      
+      let xml = X.xml_of_string body in
+      return (`Ok (get_attributes_response_of_xml encoded xml))
     with HC.Http_error (code, _, body) ->  return (error_msg code body)
  
-(* delete attributes *)
+  (* delete attributes *)
 
   let delete_attributes ?(encode=true) creds domain item attrs = 
-    let _, attrs' = 
-      List.fold_left 
-        (fun (i, acc) (name, value) -> 
-          i+1, (sprint "Attribute.%d.Name" i, (if encode then Util.base64 name else name)) 
-          :: (sprint "Attribute.%d.Value" i, (if encode then Util.base64 value else value))
-          :: acc) (0,[]) attrs in
+    let _, attrs' = List.fold_left (
+      fun (i, acc) (name, value) -> 
+        let name_p = sprint "Attribute.%d.Name" i, b64enc_if encode name in
+        let value_p = sprint "Attribute.%d.Value" i, b64enc_if encode value in
+        i+1, name_p :: value_p :: acc
+    ) (0,[]) attrs in
     let url, params = signed_request creds
       (("Action", "DeleteAttributes") 
        :: ("DomainName", domain)
-       :: ("ItemName", (if encode then Util.base64 item else item))
+       :: ("ItemName", b64enc_if encode item)
        :: attrs') in 
     try_lwt 
        lwt header, body = HC.post ~body:(`String (Util.encode_post_url params)) url in
-       
        return `Ok
-    with HC.Http_error (code, _, body) ->  return (error_msg code body)
+    with HC.Http_error (code, _, body) ->
+      return (error_msg code body)
  
-(* select *)
+  (* select: TODO if [encode=true], encode references to values in the
+     select [expression].  This might not be easy, as the [expression]
+     will have to be deconstructed (parsed). Alternatively,
+     [expression] is replaced with an expression type, which would
+     make value substitutions easier. Neither would work for numerical
+     constraints.  *)
 
   let select ?(consistent=false) ?(encoded=true) ?(token=None) creds expression =
     let url, params = signed_request ~safe:true creds
