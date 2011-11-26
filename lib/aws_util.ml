2a13c0f7dca4d3d33d96f77a4fffb5004c4a5f55
wleferrand <william@corefarm.com>
2011-11-25 16:14:47 -0800

    removing dbg messages

--- lib/aws_util.ml
+++ lib/aws_util.ml
@@ -21,21 +21,22 @@ let base64_decoder str =
 let colon_space (k, v) = k ^ ": " ^ v
 
 let encode_url ?(safe=false) str = 
-  if not safe then 
+(*  if not safe then 
     Netencoding.Url.encode ~plus:false str
-  else
+  else *)
     begin
       let strlist = ref [] in 
       for i = 0 to String.length str - 1 do 
         let c = Char.code (str.[i]) in 
         if 
           (65 <= c && c <= 90) || 
-          (48 <= c && c <= 57 ) || 
-          (97 <= c && c <= 122) || 
-          (c = 126) || 
-          (c = 95) || 
-          (c = 46) || 
-          (c = 45) 
+            (48 <= c && c <= 57 ) || 
+            (97 <= c && c <= 122) || 
+            (c = 126) || 
+            (c = 95) || 
+            (c = 46) || 
+            (c = 45) (* ||
+            (c = 47) *)
         then  
 	  strlist := Printf.sprintf "%c" str.[i] :: !strlist 
         else 
--- lib/s3_factory.ml
+++ lib/s3_factory.ml
@@ -508,7 +508,7 @@ let put_object
     ~amz_headers
     ~date 
     ~bucket 
-    ~request_uri:("/" ^ objekt) 
+    ~request_uri:("/" ^ (Util.encode_url objekt)) 
     creds
   in
   let bucket_object = (Util.encode_url bucket) ^ "/" ^ 
@@ -536,7 +536,7 @@ let put_object
     lwt () = close () in
     match exn with
     | HC.Http_error (301, _, body) -> permanent_redirect_of_string body
-    | HC.Http_error (_, _, body) -> print_endline body ; (
+    | HC.Http_error (_, _, body) ->  (
         error_msg body >>= function
           | `Error "AccessDenied" -> return `AccessDenied
           | `Error _ as err -> return err
